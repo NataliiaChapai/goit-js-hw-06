d2be3ebe01559d1a7b8064d1adac1020c18d3b1b
NataliiaChapai <natalya.chapay@gmail.com>
2022-01-14 21:11:51 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyRef = document.querySelector('body')
+const colorRef = document.querySelector('.color');
+const changeColorRef = document.querySelector('.change-color')
+
+changeColorRef.addEventListener('click', onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonClick() {
+  bodyRef.setAttribute('style', `background-color: ${getRandomHexColor()};`);
+  colorRef.textContent = getRandomHexColor();
 }
