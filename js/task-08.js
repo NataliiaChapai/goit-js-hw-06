b72a847ff1e0b47a919a8e27551983caf1f605c9
NataliiaChapai <natalya.chapay@gmail.com>
2022-01-14 19:36:10 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    event.preventDefault();
+     
+    if (email === '' || password === '') {
+        alert('Please note that all fields are required!')
+    } else {
+        const formData = {
+        email,
+        password,
+    }
+        console.log(formData);
+        
+    }
+    
+    formRef.reset()
+}
