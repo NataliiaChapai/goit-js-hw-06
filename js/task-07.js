61f49fdd336daf3a9e61c7c21ee85507bc3eb479
NataliiaChapai <natalya.chapay@gmail.com>
2022-01-16 00:06:47 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
-
+inputRef.value = 0;
 const fontSizeChange = inputRef.addEventListener('input', (event) => {
     const fontSize = event.currentTarget.value;
     textRef.setAttribute('style', `font-size: ${fontSize}px;`)
